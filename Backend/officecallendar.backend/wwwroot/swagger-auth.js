d47ad711c472219d8262f93717c33fb9baa40b82
Mex <1079546@hr.nl>
2025-12-05 14:21:57 +0100

    Fix swagger auto auth

--- Backend/officecallendar.backend/wwwroot/swagger-auth.js
+++ Backend/officecallendar.backend/wwwroot/swagger-auth.js
@@ -4,23 +4,12 @@
     window.fetch = async (input, init) => {
         const response = await original(input, init);
 
-        if (input.toString().includes("/login") && response.ok) {
+        if ((input.toString().includes("/login") || input.toString().includes("/register")) && response.ok) {
             const data = await response.clone().json();
             const token = data.token;
 
-            if (token) {
-                const interval = setInterval(() => {
-                    const ui = sessionStorage.getItem("swagger-ui-authorization");
-
-                    SwaggerUIBundle({
-                        requestInterceptor: (req) => {
-                            req.headers["Authorization"] = "Bearer " + token;
-                            return req;
-                        }
-                    });
-
-                    clearInterval(interval);
-                }, 500);
+            if (token && window.ui) {
+                window.ui.preauthorizeApiKey("Bearer", token);
             }
         }
 
